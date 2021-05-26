b3c788354f0714a8f47793cb43989fca31c06a5b
lee-tae-eon <mktears0609@gmail.com>
2021-05-26 17:18:05 +0900

    challenge and middleware

--- src/server.js
+++ src/server.js
@@ -4,6 +4,20 @@ const app = express();
 
 const PORT = 4000;
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  console.log(`${req.protocol}`);
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("i am final");
+};
+
+app.use(logger);
+
+app.get("/", handleHome);
+
 const handelListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT}`);
 
