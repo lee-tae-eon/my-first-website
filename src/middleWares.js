d82a3c50c073d72841ece3ed35dc6c30a1e7bbb2
lee-tae-eon <mktears0609@gmail.com>
2021-06-30 11:33:40 +0900

    isHeroku local

--- src/middleWares.js
+++ src/middleWares.js
@@ -34,6 +34,7 @@ export const localsMiddleWare = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "gyumTube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
