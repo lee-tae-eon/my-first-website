dadf4baeebf5fadea101d131bfbebed670a95b9b
lee-tae-eon <mktears0609@gmail.com>
2021-07-22 01:10:05 +0900

    homechange

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -1,3 +1,9 @@
-export const home = (req, res) => {
-  return res.render("homepage", { pageTitle: "HOME" });
+import Photo from "../models/Photo";
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  const videos = await Video.find({}).populate("owner");
+  const photos = await Photo.find({}).populate("owner");
+
+  return res.render("homepage", { pageTitle: "HOME", videos, photos });
 };
